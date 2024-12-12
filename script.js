6babe37e4c529030eed3ad39257dbecaed6ded1b
rob467 <133133177+rob467@users.noreply.github.com>
2024-12-12 10:39:34 +0100

    Displays library contents on page

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const bookDisplay = document.querySelector(".book-display")
+
+
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -5,9 +8,29 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function() {
-        return `${title} by ${author}, ${pages} pages, ${read ? 'read' : 'not read yet'}`
+}
+function addBookToLibrary(title, author, pages, read) {
+    let newBook = new Book(title, author, pages, read)
+    myLibrary.push(newBook)
+}
+
+addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 293, read=false)
+addBookToLibrary('Harry Potter', 'J.K. Rowling', 220, read=false)
+addBookToLibrary('Game of Thrones', 'G.R.R. Martin', 694, read=false)
+
+function displayLibrary() {
+    if (!myLibrary) {
+        alert('Nothing to see hear!')
+        return
+    }
+    for (const book of myLibrary) {
+        const bookDiv = document.createElement("div")
+        bookDiv.classList.add("book-card");
+        bookDiv.textContent = `${book.title}`
+        bookDisplay.appendChild("bookDiv")
     }
 }
 
-function addBookToLibrary() {}
+
+
+displayLibrary()
